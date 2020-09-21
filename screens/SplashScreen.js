8d9b3c2efe5170d52bec7e78b55bdd4d3445afba
Ricardo Salguero <rsalguero3@yahoo.com>
2020-09-21 15:10:09 -0500

    Updated Splash Screen and added icons
    updated splash screen to retreive questions from server. added an error message if it is received from the server. also added icons for the chat ui

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {StatusBar, View} from 'react-native';
+import {StatusBar, Text, View, Button} from 'react-native';
+import OnboardingRepository from '../onboarding/OnboardingRepository';
 import styles from './SplashStyle';
 import SplashLogo from '../assets/svg/SplashLogo';
 import COLOR from '../constant/colors';
@@ -7,14 +8,49 @@ import COLOR from '../constant/colors';
 export default class SplashScreen extends Component {
   constructor(props) {
     super(props);
+    this.onboardingRepository = OnboardingRepository.getInstance();
+    this.state = {
+      hideError: true,
+      errorMessage: '',
+    };
+    this.fetchQuestions = this.fetchQuestions.bind(this);
+  }
+
+  fetchQuestions() {
+    this.setState({
+      hideError: true,
+    });
+    this.onboardingRepository
+      .getOnboardingQuestions()
+      .then((questions) => {
+        this.props.navigation.navigate('OnboardingScreen', {questions});
+      })
+      .catch((error) => {
+        this.setState({hideError: false, errorMessage: error.toString()});
+      });
+  }
+  componentDidMount() {
+    this.fetchQuestions();
   }
 
   render() {
+    const {hideError, errorMessage} = this.state;
+    const display = hideError ? 'none' : 'flex';
+
     return (
       <View style={styles.container}>
         <StatusBar backgroundColor={COLOR.WHITE} barStyle="dark-content" />
         <View style={styles.splashLogo}>
           <SplashLogo width={200} height={200} />
+        </View>
+        <View style={{display}}>
+          <Text>{errorMessage}</Text>
+          <Button
+            onPress={this.fetchQuestions}
+            title="Try Again"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
         </View>
       </View>
     );
